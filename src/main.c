f9f4fdc287aa25c2c7e6c905a08fe5b69da0d01f
OnurKader <onurorkunkader1999@gmail.com>
2019-10-31 19:33:10 +0000

    Ugly but it works, sleep sucks

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
@@ -16,8 +17,11 @@ const char* usage();
 void cls();
 void signal_handle(int);
 void get_time(time_t*, struct tm*);
-void print_time(const int, struct tm*);
+void print_time(const int, struct tm*, struct winsize*);
 void display_inputs(int);
+void gotoRC(int row, int col);
+
+int _rows, _cols;
 
 int main(int argc, char** argv)
 {
@@ -25,7 +29,11 @@ int main(int argc, char** argv)
 	signal(SIGTERM, signal_handle);
 
 	display_inputs(0);	  // Disable echo'ing chars while running
-	char* time_buffer = calloc(1, 2);
+
+	struct winsize size;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+	_rows = size.ws_row;
+	_cols = size.ws_col;
 
 	int optc = 0;
 	uint8_t color = 7U;
@@ -41,13 +49,13 @@ int main(int argc, char** argv)
 	time_t raw;
 	struct tm timeinfo;
 
+	cls();
 	printf("\033[?25l");
 	while(1)
 	{
-		cls();
+		/* cls(); */
 		get_time(&raw, &timeinfo);
-		print_time(color, &timeinfo);
-		sleep(1U);
+		print_time(color, &timeinfo, &size);
 	}
 	return 0;
 }
@@ -60,17 +68,6 @@ void signal_handle(int signum)
 {
 	printf("%d\033[?25h", signum);
 	cls();
-	printf("%s\n\n", (numbers[0]));
-	printf("%s\n\n", (numbers[1]));
-	printf("%s\n\n", (numbers[2]));
-	printf("%s\n\n", (numbers[3]));
-	printf("%s\n\n", (numbers[4]));
-	printf("%s\n\n", (numbers[5]));
-	printf("%s\n\n", (numbers[6]));
-	printf("%s\n\n", (numbers[7]));
-	printf("%s\n\n", (numbers[8]));
-	printf("%s\n\n", (numbers[9]));
-	printf("%s\n", (numbers[10]));
 	exit(0);
 }
 
@@ -80,13 +77,74 @@ void get_time(time_t* raw, struct tm* timeinfo)
 	*timeinfo = *localtime(raw);
 }
 
-void print_time(const int color, struct tm* timeinfo)
+void print_time(const int color, struct tm* timeinfo, struct winsize* size)
 {
-	printf("\033[%dm%02d:%02d:%02d\033[m\n",
-		   29 + color,
-		   timeinfo->tm_hour,
-		   timeinfo->tm_min,
-		   timeinfo->tm_sec);
+	int num_indices[2];
+	num_indices[0] = timeinfo->tm_hour / 10;
+	num_indices[1] = timeinfo->tm_hour % 10;
+	const int row = size->ws_row / 2 - 3;
+	const int col = (size->ws_col - 72) / 2;
+	gotoRC(row, col);
+	for(int j = 0; j < 2; ++j)
+		for(int i = 0; i < 7; ++i)
+		{
+			gotoRC(row + i, col + 9 * j);
+			printf("\033[%dm%s\033[m", (color + 29), numbers[num_indices[j]][i]);
+		}
+	for(int i = 0; i < 7; ++i)
+	{
+		gotoRC(row + i, col + 18);
+		printf("\033[%dm%s\033[m", (color + 29), numbers[10][i]);
+	}
+
+	/////////////////////////////////////////////////
+	num_indices[0] = timeinfo->tm_min / 10;
+	num_indices[1] = timeinfo->tm_min % 10;
+	for(int i = 0; i < 7; ++i)
+	{
+		gotoRC(row + i, col + 27);
+		printf("\033[%dm%s\033[m", (color + 29), numbers[num_indices[0]][i]);
+	}
+	for(int i = 0; i < 7; ++i)
+	{
+		gotoRC(row + i, col + 36);
+		printf("\033[%dm%s\033[m", (color + 29), numbers[num_indices[1]][i]);
+	}
+	for(int i = 0; i < 7; ++i)
+	{
+		gotoRC(row + i, col + 45);
+		printf("\033[%dm%s\033[m", (color + 29), numbers[10][i]);
+	}
+
+	/////////////////////////////////////////////////
+	num_indices[0] = timeinfo->tm_sec / 10;
+	num_indices[1] = timeinfo->tm_sec % 10;
+	for(int i = 0; i < 7; ++i)
+	{
+		gotoRC(row + i, col + 54);
+		printf("\033[%dm%s\033[m", (color + 29), numbers[num_indices[0]][i]);
+	}
+	for(int i = 0; i < 7; ++i)
+	{
+		gotoRC(row + i, col + 63);
+		printf("\033[%dm%s\033[m", (color + 29), numbers[num_indices[1]][i]);
+	}
+	// Quarter second delay 0.25s
+	usleep(250000U);
+	/* sprintf(buffer, */
+	/* 		"\033[%dm%02d:%02d:%02d\033[m\n", */
+	/* 		(29 + color), */
+	/* 		timeinfo->tm_hour, */
+	/* 		timeinfo->tm_min, */
+	/* 		timeinfo->tm_sec); */
+	if(size->ws_row != _rows || size->ws_col != _cols)
+	{
+		cls();
+		_rows = size->ws_row;
+		_cols = size->ws_col;
+	}
+	else
+		ioctl(STDOUT_FILENO, TIOCGWINSZ, size);
 }
 
 void display_inputs(
@@ -119,4 +177,6 @@ void display_inputs(
 		return;
 	}
 }
+
+void gotoRC(int row, int col) { printf("\033[%d;%dH", row, col); }
 
